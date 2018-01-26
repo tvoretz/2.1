0ed24a805434dc9cf651e50f94472475ab132a4c
Dmitry Urvantsev <d.urvantsev@gmail.com>
2018-01-26 23:43:37 +0500

    Initial commit

---
+++ homework.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+# cook_book = {
+#     'яйчница': [
+#         {'ingridient_name': 'яйца', 'quantity': 2, 'measure': 'шт.'},
+#         {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'}
+#     ],
+#     'стейк': [
+#         {'ingridient_name': 'говядина', 'quantity': 300, 'measure': 'гр.'},
+#         {'ingridient_name': 'специи', 'quantity': 5, 'measure': 'гр.'},
+#         {'ingridient_name': 'масло', 'quantity': 10, 'measure': 'мл.'}
+#     ],
+#     'салат': [
+#         {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'},
+#         {'ingridient_name': 'огурцы', 'quantity': 100, 'measure': 'гр.'},
+#         {'ingridient_name': 'масло', 'quantity': 100, 'measure': 'мл.'},
+#         {'ingridient_name': 'лук', 'quantity': 1, 'measure': 'шт.'}
+#     ]
+# }
+
+def read_cookbook():
+
+    cook_book = {}
+
+    with open('cookbook.txt') as f:
+        for line in f:
+            dish_name = line.strip()
+            ingridients_num = int(f.readline().strip())
+
+            for i in range(ingridients_num):
+                ingridient_list = f.readline().strip().split(' | ')
+                cook_book[dish_name][i] = {'ingridient_name': 1, 'quantity': 2, 'measure': 3}
+
+                #print(ingridient_list)
+
+            # прочитать пустую строку
+            f.readline()
+
+            #cook_book[dish_name] = ingridients_num
+        print(cook_book)
+
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        for ingridient in cook_book[dish]:
+            new_shop_list_item = dict(ingridient)
+
+            new_shop_list_item['quantity'] *= person_count
+            if new_shop_list_item['ingridient_name'] not in shop_list:
+                shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+            else:
+                shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
+    return shop_list
+
+
+def print_shop_list(shop_list):
+    for shop_list_item in shop_list.values():
+        print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'],
+                                shop_list_item['measure']))
+
+
+def create_shop_list():
+    person_count = int(input('Введите количество человек: '))
+    dishes = input('Введите блюда в расчете на одного человека (через запятую): ') \
+        .lower().split(', ')
+    shop_list = get_shop_list_by_dishes(dishes, person_count)
+    print_shop_list(shop_list)
+
+
+#create_shop_list()
+
+read_cookbook()
